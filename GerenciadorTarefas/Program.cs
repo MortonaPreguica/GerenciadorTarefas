c808888cdfb19bdd9ff643a9359ff0ae51cac9f3
Raul Rezende <raul.rezende@libercapital.com.br>
2023-09-27 23:00:48 -0300

    criando o primeiro passo de adicionar tarefa

--- GerenciadorTarefas/Program.cs
+++ GerenciadorTarefas/Program.cs
@@ -1,2 +1,50 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+Console.WriteLine("Bem-vindo a seu Gerenciador de tarefas. Para começar digite o comando: ");
+
+
+string? comando = "0";
+
+List<string> listaTarefas = new();
+
+while(comando != "9")
+{
+    if (comando != null)
+    {
+        if(comando.Equals("0"))
+        {
+            Console.WriteLine("1 - Adicionar tarefa. ");
+            Console.WriteLine("2 - Listar tarefa. ");
+            Console.WriteLine("3 - Editar tarefa. ");
+            Console.WriteLine("4 - Excluir tarefa. ");
+            Console.WriteLine("Digite o comando:");
+
+            comando = Console.ReadLine();
+        }
+        
+        if (comando == "1")
+        {
+            Console.WriteLine("Adicione o nome da tarefa: ");
+            string? novaTarefa = Console.ReadLine();
+
+            if (novaTarefa != null)
+            {
+                listaTarefas.Add(novaTarefa);
+                Console.WriteLine("Tarefa criada com sucesso!");
+                comando = "0";
+            }
+
+        }
+        else if (comando == "2")
+        {
+            Console.WriteLine("Lista de tarefas");
+
+            for (int i = 0; i <= listaTarefas.Count; i++)
+            {
+                Console.WriteLine($"{i} - {listaTarefas[i]} ");
+            }
+
+            Console.WriteLine("Finalizou!");
+        }
+    }
+}
+
